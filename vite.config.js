024b4ad02a188f5279f74c6b9fe9961f6ab363f5
Aluno <agatha.erika06@gmail.com>
2026-08-10 16:21:07 -0300

    corrige caminho do GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/meu-curriculo/' // Adicione esta linha para definir o caminho base do seu projeto
+  base: '/reactgithub/' // Adicione esta linha para definir o caminho base do seu projeto
 })
